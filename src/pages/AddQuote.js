d8a94588e7271412a1c7197cbe414f211c48bf56
rakib2067 <rakib7@hotmail.co.uk>
2021-11-18 17:24:23 +0000

    added programmatic navigation through useHistory

--- src/pages/AddQuote.js
+++ src/pages/AddQuote.js
@@ -1,8 +1,11 @@
 import React from "react";
+import { useHistory } from "react-router";
 import QuoteForm from "../components/quotes/QuoteForm";
 export default function AddQuote() {
+  const history = useHistory();
   const addQuoteHandler = (e) => {
     console.log("hello");
+    history.push("/quotes");
   };
   return <QuoteForm onAddQuote={addQuoteHandler} />;
 }
